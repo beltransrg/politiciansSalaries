1b81feb61effc484be4895bcb6bdd846e2d2f3e2
Borja López <borja.lopez.gomez@gmail.com>
2021-11-03 20:47:42 +0100

    first draft of scraper code, solving javascript issues by using selenium + webdriver (firefox)

--- pec1src/scraper.py
+++ pec1src/scraper.py
@@ -1,15 +1,108 @@
-#test
-def download(url, num_retries=2):
-    import urllib.request 
-    import urllib.error    
-    print('Downloading:', url)
-    try:
-        html = urllib.request.urlopen(url).read()
-    except urllib.error.URLError as e:
-        print('Download error:', e.reason)
-        html = None
-        if num_retries > 0:
-            if hasattr(e, 'code') and 500 <= e.code < 600:
-            # recursively retry 5xx HTTP errors
-                return download(url, num_retries-1)
-    return html
+from typing import Sized
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+last_active_page = ''
+
+def process_person_in_new_tab(url):
+
+    # Open a new window
+    driver.execute_script("window.open('');")
+
+    # Switch to the new window
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get(url)
+    time.sleep(1)
+
+    # TODO extract information from person
+
+    # nombre
+
+    # cargo (job_title)
+
+    # partido (affiliation)
+
+    # fecha nacimiento
+
+    # institucion
+
+    # biografia
+
+    # historico (V2?)
+
+    # foto (V2?) => aqui parece que hay derechos y demas
+
+    # bruto anual
+
+    # bruto mensual
+
+    # close the active tab
+    driver.close()
+
+    # Switch back to the first tab
+    driver.switch_to.window(driver.window_handles[0])
+
+def process_page():
+
+    # getting active page to check when the last page has been reached
+    active_page_selector = '#advanced-search > section.b-results > div.e-pagination > div > div > div > div > ul > li.active'
+    active_page = driver.find_element_by_css_selector(active_page_selector)
+    global last_active_page
+    if active_page.text != last_active_page: 
+
+        print(f"Processing page {active_page.text}")
+        last_active_page = active_page.text
+        persons = driver.find_elements_by_class_name('e-result')
+        print(len(persons))
+
+        # looping over persons
+        for person in persons:
+            #product_name = result.text
+            link = person.find_element_by_tag_name('a')
+            url_link = link.get_attribute("href")
+            process_person_in_new_tab(url_link)
+            #print(url_link)
+
+        # return value to keep going on
+        return True
+    else:
+        print(f"Last page {active_page} has been reached")
+
+        # return value to stop processsing
+        return False
+
+    
+
+# does not work
+# r = requests.get('https://transparentia.newtral.es/busqueda-avanzada?name=&inactive=true')
+# soup = BeautifulSoup(r.text, 'lxml')
+# print(soup.html)
+# persons = soup.find_all('div', class_='e-result')
+# print(persons)
+
+# trying to use Selenium
+print("initializing webdriver")
+driver = webdriver.Firefox()
+
+print("getting url")
+url = 'https://transparentia.newtral.es/busqueda-avanzada?name=&inactive=true'
+driver.get(url)
+
+print("sleeping")
+time.sleep(2)
+
+# processing one page (TODO loop)
+while process_page():
+    
+    # next page
+    next_button_selector = '#advanced-search > section.b-results > div.e-pagination > div > div > div > div > a.next'
+    next_button = driver.find_element_by_css_selector(next_button_selector)
+    next_button.click()
+    time.sleep(2)
+
+print("quitting")
+driver.quit()
+
+
