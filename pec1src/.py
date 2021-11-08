bd8fb8eda9b5ca21928fe096770df7a00e99784e
Borja López <borja.lopez.gomez@gmail.com>
2021-11-08 19:18:03 +0100

    removing useless stuff

--- pec1src/.py
+++
@@ -1,8 +0,0 @@
-def crawl_sitemap(url):
-	 # download the sitemap file
-	 sitemap = download(url)
-	 # extract the sitemap links
-	 links = re.findall('<loc>(.*?)</loc>', sitemap)
-	 # download each link
-	 for link in links:
-        	print("link: " + "-" + link)
